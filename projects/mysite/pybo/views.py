f9684676e969610465c62f9216e5fdd94c7eaf92
seojilee <seojilee@student.42seoul.kr>
2025-01-15 14:02:40 +0900

    add url and templates for questions

--- projects/mysite/pybo/views.py
+++ projects/mysite/pybo/views.py
@@ -1,7 +1,14 @@
-from django.shortcuts import render
+from django.shortcuts import render, get_object_or_404
 
 # Create your views here.
-from django.http import HttpResponse
+from .models import Question
 
 def index(request):
-    return HttpResponse("안녕하세요. pybo에 오신 걸 환영합니다.")
+    question_list = Question.objects.order_by('-create_date')
+    context = {'question_list': question_list}
+    return render(request, 'pybo/question_list.html', context)
+
+def detail(request, question_id):
+    question = get_object_or_404(Question, pk=question_id)
+    context = {'question': question}
+    return render(request, 'pybo/question_detail.html', context)
