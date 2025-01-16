51d2af7c4d4a29a3513c2e02ef8263f94b23dfbf
seojilee <seojilee@student.42seoul.kr>
2025-01-16 17:32:22 +0900

    remove widgets attr

--- projects/mysite/pybo/forms.py
+++ projects/mysite/pybo/forms.py
@@ -14,10 +14,10 @@ class   QuestionForm(forms.ModelForm):
     class   Meta:
         model = Question
         fields = ['subject', 'content']
-        widgets = {
-            'subject': forms.TextInput(attrs={'class': 'form-control'}),
-            'content': forms.Textarea(attrs={'class': 'form-control', 'rows': 10}),
-        }
+#        widgets = {
+#            'subject': forms.TextInput(attrs={'class': 'form-control'}),
+#            'content': forms.Textarea(attrs={'class': 'form-control', 'rows': 10}),
+#        }
         labels = {
             'subject': '제목',
             'content': '내용',
