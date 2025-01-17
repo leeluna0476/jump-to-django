1ed905ae85995717d2dc54b2e36ae362f187f0b9
seojilee <seojilee@student.42seoul.kr>
2025-01-17 13:16:15 +0900

    remove paging temporarily

--- pybo/views.py
+++ pybo/views.py
@@ -3,16 +3,17 @@ from django.shortcuts import render, get_object_or_404, redirect
 # Create your views here.
 from django.utils import timezone
 from django.http import HttpResponseNotAllowed
-from django.core.paginator import Paginator
+#from django.core.paginator import Paginator
 from .models import Question, Answer
 from .forms import QuestionForm, AnswerForm
 
 def index(request):
     page = request.GET.get('page', '1')
     question_list = Question.objects.order_by('-create_date')
-    paginator = Paginator(question_list, 10)
-    page_obj = paginator.get_page(page)
-    context = {'question_list': page_obj}
+#    paginator = Paginator(question_list, 10)
+#    page_obj = paginator.get_page(page)
+#    context = {'question_list': page_obj}
+    context = {'question_list': question_list}
     return render(request, 'pybo/question_list.html', context)
 
 def detail(request, question_id):
